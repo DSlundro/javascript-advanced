5536f42a6a102a94244479bc3a38b4b1ff20239b
DSlundro <dragan.savic95@outlook.it>
2022-09-01 19:12:25 +0200

    sezione 9 - data structures

--- prove/app.js
+++ prove/app.js
@@ -1,8 +1,57 @@
-const arr = [1,2,4]
-const arr2 = [4,5,6]
-const arr3 = [[2,3],1,4,[5,6]]
-const arr4 = [4, 1, 6, 2, 3]
+'use strict'
 
-console.log(arr.reverse());
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Carpese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri:  {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+    order: function(starterIndex, mainIndex){
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
+    orderDelivery: function ({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(`
+        Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+        will be delivered to ${address} 
+        at ${time}`);
+    },
+};
 
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+})
+
+restaurant.orderDelivery({
+    address: 'Via Pietro Maroncelli, 3',
+    starterIndex: 1,
+})
+
+
+
+
+const { openingHours } = restaurant
+const { fri: {open, close} } = openingHours
+console.log(open, close);
 
